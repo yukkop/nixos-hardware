b4497d9a077c777d9a7941517b7ef5045c3e873b
yukkop <hectic.yukkop@gmail.com>
2024-07-07 12:54:20 +0000

    fix: 15arh7

--- lenovo/ideapad/15arh7/default.nix
+++ lenovo/ideapad/15arh7/default.nix
@@ -3,23 +3,18 @@
 {
   imports = [
     ../../../common/cpu/amd
+    ../../../common/cpu/amd/pstate.nix
+    ../../../common/gpu/amd
     ../../../common/gpu/nvidia/prime-sync.nix
     ../../../common/pc/laptop
-    ../../../common/pc/laptop/acpi_call.nix
     ../../../common/pc/laptop/ssd
   ];
 
-  # Specify bus id of Nvidia and Intel graphics.
-  hardware.nvidia.prime = {
-    amdgpuBusId = "PCI:5:0:0";
-    nvidiaBusId = "PCI:1:0:0";
-  };
-
-  # Cooling management
-  services.thermald.enable = lib.mkDefault true;
-
-  # tlp defaults to "powersave", which doesn't exist on this laptop
-  services.tlp.settings = {
-    CPU_SCALING_GOVERNOR_ON_BAT = "schedutil";
+  hardware.nvidia = {
+    modesetting.enable = true;
+    prime = {
+      amdgpuBusId = "PCI:5:0:0";
+      nvidiaBusId = "PCI:1:0:0";
+    };
   };
 }
